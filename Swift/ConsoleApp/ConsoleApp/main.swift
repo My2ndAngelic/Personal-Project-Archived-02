c39c39968df6c4fe5ac9189dd0a92fb6378aa76f
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2022-03-10 13:51:36 +0100

    AAA

---
+++ Swift/ConsoleApp/ConsoleApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConsoleApp
+//
+//  Created by My2ndAngelic on 10/03/2022.
+//  Copyright © 2022 My2ndAngelic. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
