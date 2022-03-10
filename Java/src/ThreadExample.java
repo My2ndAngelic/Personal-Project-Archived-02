567a5f6918e21ddbb5354d2f43e9a2ad5d64ceda
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2022-03-10 01:19:45 +0100

    Update

---
+++ Java/src/ThreadExample.java
@@ -0,0 +1,36 @@
+import java.util.stream.LongStream;
+
+public class ThreadExample extends Thread{
+    public static long addSingleThread(long start, long end) {
+//        int result = 0;
+//        for (int i = start; i <= end; i++) {
+//            result += i;
+//        }
+//        return result;
+        return LongStream.range(start, end+1).sum();
+
+    }
+
+    public static long addDoubleThread(long start, long end) {
+//        Thread t1 = new Thread(() -> {
+//            for (int i = start; i <= end/2; i++) {
+//                result[0] += i;
+//            }
+//        });
+//
+//        Thread t2 = new Thread(() -> {
+//            for (int j = end/2+1; j <= end; j++) {
+//                result[0] += j;
+//            }
+//        });
+//
+//        t1.start();
+//        t2.start();
+        return LongStream.range(start, end+1).parallel().sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addSingleThread(1, 99));
+        System.out.println(addDoubleThread(1, 99));
+    }
+}
---
+++ Java/src/ThreadExampleTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThreadExampleTest {
+    private long start = 1;
+    private long end = 6942044;
+    private long result = (start + end) * (end - start + 1) /2;
+
+    @Test
+    void addSingleThread() {
+        assertEquals(result, ThreadExample.addSingleThread(start, end));
+    }
+
+    @Test
+    void addDoubleThread() {
+        assertEquals(result, ThreadExample.addDoubleThread(start, end));
+    }
+}
